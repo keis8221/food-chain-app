db65c62b3f21c15e8a20fdb77720116f3b0337ef
keis8221 <kei_s8221@icloud.com>
2022-12-07 15:22:46 +0900

    chore: import routify

---
+++ web/app/.routify/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "childProcess": "dev:vite",
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2022-12-07T06:18:37.216Z"
+}
