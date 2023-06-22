d3472e4c797b59267b9b006dff2d16a105712e55
YusukeWat <yuusuke.watanabe1@jp.ricoh.com>
2023-06-22 20:52:07 +0900

    予約ステータス変更を実装(ただしボタンの配置や確認ダイアログ表示、配送者選択などは未)

--- web/app/src/pages/reservation/[id]/index.svelte
+++ web/app/src/pages/reservation/[id]/index.svelte
@@ -1,22 +1,122 @@
 <script lang="ts">
   import { goto } from "@roxi/routify";
   import { addToast } from "../../../stores/Toast";
-  import { ReservationRepository } from "../../../models/Reservation";
+  import { ReservationRepository, type TReservation } from "../../../models/Reservation";
   import CircularProgress from "@smui/circular-progress";
   import { params } from "@roxi/routify";
   import dayjs from "dayjs";
+  import Button from "@smui/button";
   import { Content } from "@smui/dialog";
   import StatusLabel from ".././_components/StatusLabel.svelte";
   import { markAsLogoutState } from "../../../stores/Login";
   import { CROP_UNITS_LABEL } from "../../../constants/product";
+  import { profile } from "../../../stores/Account";
+  
+  // 仮置き。reservation_index取り込んだら消えるよ。
+  const USER_ATTRIBUTE = {
+    producer: "producer",
+    consumer: "consumer",
+    logistics: "logistics",
+    intermediary: "intermediary",
+  };
 
   $: reservationRepository = new ReservationRepository();
+  
+  let reservationStatus: TReservation["status"];
 
   async function fetchReservationProducts() {
     try {
-      return await reservationRepository.findOne($params.id);
+      const reservationData = await reservationRepository.findOne($params.id);
+      reservationStatus = reservationData.status;
+      return reservationData;
     } catch (err) {
-      switch (err.error || err.message) {
+      handleError(err);
+      return null;
+    }
+  }
+
+  async function packed() {
+    console.log("出荷ボタンが押された");
+    // ここのガード処理いるか？ボタンのvisiblityで充分じゃないか？
+    if ($profile.attribute !== USER_ATTRIBUTE.producer) {
+      addToast({
+        message:
+          "無効なユーザー属性です。",
+        type: "error",
+      });
+      return;
+    }
+
+    // ひとまず生産者自身を配送者に設定しとく
+    const shipperId = $profile.id;
+
+    try {
+      const updateReservationData = await reservationRepository.packed($params.id, {shipperId: shipperId});
+      reservationStatus = updateReservationData.status;
+      addToast({
+        message:
+          "予約商品の出荷が完了しました。",
+      });
+    } catch(err) {
+      handleError(err);
+    }
+  }
+
+  async function kept() {
+    console.log("店舗預かりボタンが押された");
+    // ここのガード処理いるか？ボタンのvisiblityで充分じゃないか？
+    if ($profile.attribute !== USER_ATTRIBUTE.intermediary) {
+      addToast({
+        message:
+          "無効なユーザー属性です。",
+        type: "error",
+      });
+      return;
+    }
+
+    try {
+      const updateReservationData = await reservationRepository.kept($params.id);
+      reservationStatus = updateReservationData.status;
+      addToast({
+        message:
+          "予約商品の店舗預かりが完了しました。",
+      });
+    } catch(err) {
+      handleError(err);
+    }
+  }
+
+  async function received() {
+    console.log("受取りボタンが押された");
+    // ここのガード処理いるか？ボタンのvisiblityで充分じゃないか？
+    if ($profile.attribute !== USER_ATTRIBUTE.consumer) {
+      addToast({
+        message:
+          "無効なユーザー属性です。",
+      });
+      return;
+    }
+
+    try {
+      const updateReservationData = await reservationRepository.received($params.id);
+      reservationStatus = updateReservationData.status;
+      addToast({
+        message:
+          "予約商品の受取りを完了しました。",
+      });
+    } catch(err) {
+      handleError(err);
+    }
+  }
+
+  function handleError(err) {
+    switch (err.error || err.message) {
+        case "Bad Request":
+          addToast({
+            message: "予約の更新に失敗しました。開発者へお問い合わせください。", // 400 Bad Request が返る = ガード条件不備のフロントエンド実装ミス
+            type: "error",
+          });
+          break;
         case "Unauthorized":
           markAsLogoutState();
           addToast({
@@ -28,13 +128,11 @@
         default:
           addToast({
             message:
-              "予約の取得に失敗しました。もう一度時間をおいて再読み込みしてください。",
+              "予約の更新に失敗しました。もう一度時間をおいて再読み込みしてください。",
             type: "error",
           });
           break;
       }
-      return null;
-    }
   }
 </script>
 
@@ -71,7 +169,7 @@
             {reservationData.product.name}
           </p>
           <p class="status-label">
-            <StatusLabel status={reservationData.status} />
+            <StatusLabel bind:status={reservationStatus} />
           </p>
         </div>
       </div>
@@ -133,6 +231,31 @@
           </tbody>
         </table>
       </div>
+      <!-- ボタンは並べただけ。 -->
+      <Button
+        class="mt-14 px-7 h-12"
+        color="secondary"
+        variant="raised"
+        on:click={packed}
+      >
+        <p class="text-lg font-bold">出荷</p>
+      </Button>
+      <Button
+        class="mt-14 px-7 h-12"
+        color="secondary"
+        variant="raised"
+        on:click={kept}
+      >
+        <p class="text-lg font-bold">店舗預かり</p>
+      </Button>
+      <Button
+        class="mt-14 px-7 h-12"
+        color="secondary"
+        variant="raised"
+        on:click={received}
+      >
+        <p class="text-lg font-bold">受取り</p>
+      </Button>
     </div>
   </Content>
 {/await}
