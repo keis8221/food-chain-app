869be4d3ac3f827a05353faf7c68f1e2860cef4d
keis8221 <kei_s8221@icloud.com>
2023-06-29 14:54:51 +0900

    fix: 指摘事項の修正（１点を除く）

--- web/app/src/pages/product/index.svelte
+++ web/app/src/pages/product/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { goto } from "@roxi/routify";
-  import Card, { Content, PrimaryAction, MediaContent } from "@smui/card";
+  import Card, { Content, PrimaryAction } from "@smui/card";
   import Button from "@smui/button";
   import { ProductRepository, type TProduct } from "../../models/Product";
   import CircularProgress from "@smui/circular-progress";
@@ -49,6 +49,7 @@
         color="secondary"
         variant="raised"
         class="w-[150px] px-4 py-2 mt-10 rounded-full"
+        on:click={() => $goto("./new")}
       >
         <p class="black">出品</p>
       </Button>
@@ -56,72 +57,76 @@
 
     <div class="container my-12 mx-auto px-4 md:px-12">
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      {#each products as product}
-        <div class="my-1 px-1 sm:flex sm:justify-center sm:w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 xl:w-1/4 flex width=270px max-width=100%">
-          <Card class="rounded-[24px] width=270px max-width=100% "on:click={$goto(`./${product.id}`)} >
-            <Content class="mdc-typography--body2 relative top-[10px]">
-              <div class="">
-                <!-- TODO: imageタグ内の重複コードを解消する -->
-                {#if product.producer.image}
-                  <img
-                    class="absolute top-0 w-[30px] h-[30px] rounded-[50%]"
-                    src={product.producer.image}
-                    alt=""
-                  />
-                {:else if product.producer.classification === "corporate"}
-                  <img
-                    class="absolute top-0 w-[30px] h-[30px] rounded-[50%]"
-                    src="./../../../../public/images/house.png"
-                    alt=""
-                  />
-                {:else}
-                  <img
-                    class="absolute top-0 w-[30px] h-[30px] rounded-[50%]"
-                    src="./../../../public/images/farmer.png"
-                    alt=""
-                  />
-                {/if}
-                <div
-                  class="absolute text-sm top-[6%] left-[60px] text-[#4A4A4A]"
-                >
-                  {product.producer.name}
-
-                </div>
-                <div class="text-xl font-bold mt-4"style="text-overflow: ellipsis max-width=100%">
-                  {product.name}
-                </div>
-              </div>
-            </Content>
-            <PrimaryAction on:click={$goto(`./${product.id}`)} class="h-full">
-              <div>
-                <img
-                  class="block h-48 w-96 object-contain"
-                  src={product.image ??
-                    "https://girlydrop.com/wp-content/uploads/post/p5774.jpg"}
-                  alt=""
-                />
-              </div>
-              <Content class="mdc-typography--body2 relative">
-                <div class="flex justify-center">
-                  <div class="text-lg text-[#4A4A4A] mt-1">
-                    {product.unitQuantity}{CROP_UNITS_LABEL[product.unit]}
+        {#each products as product}
+          <div
+            class="my-1 px-1 sm:flex sm:justify-center sm:w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 xl:w-1/4 flex"
+          >
+            <Card class="rounded-[24px]">
+              <PrimaryAction on:click={$goto(`./${product.id}`)} class="h-full">
+                <Content class="mdc-typography--body2 relative top-[10px]">
+                  <div class="max-w-[100%]">
+                    <!-- TODO: imageタグ内の重複コードを解消する -->
+                    {#if product.producer.image}
+                      <img
+                        class="absolute top-0 w-[30px] h-[30px] rounded-[50%]"
+                        src={product.producer.image}
+                        alt=""
+                      />
+                    {:else if product.producer.classification === "corporate"}
+                      <img
+                        class="absolute top-0 w-[30px] h-[30px] rounded-[50%]"
+                        src="./../../../../public/images/house.png"
+                        alt=""
+                      />
+                    {:else}
+                      <img
+                        class="absolute top-0 w-[30px] h-[30px] rounded-[50%]"
+                        src="./../../../public/images/farmer.png"
+                        alt=""
+                      />
+                    {/if}
+                    <div
+                      class="absolute text-sm top-[6%] left-[60px] text-[#4A4A4A]"
+                    >
+                      {product.producer.name}
+                    </div>
+                    <div
+                      class="text-xl font-bold mt-4"
+                      style="text-overflow: ellipsis max-width=100%"
+                    >
+                      {product.name}
+                    </div>
                   </div>
-                  <div class="text-2xl text-[#4A4A4A] ml-5">
-                    {product.unitPrice} 円
+                </Content>
+                <div>
+                  <img
+                    class="block h-48 w-96 object-contain"
+                    src={product.image ??
+                      "./../../../public/images/default_product_image.png"}
+                    alt=""
+                  />
+                </div>
+                <Content class="mdc-typography--body2 relative">
+                  <div class="flex justify-center">
+                    <div class="text-lg text-[#4A4A4A] mt-1">
+                      {product.unitQuantity}{CROP_UNITS_LABEL[product.unit]}
+                    </div>
+                    <div class="text-2xl text-[#4A4A4A] ml-5">
+                      {product.unitPrice} 円
+                    </div>
                   </div>
-                </div>
-                <div class="text-lg text-[#4A4A4A] mt-1 flex justify-center" >
-                  予約期間：
-                <div >{dayjs(product.startAt).format("MM/DD")}</div>
-                <div >〜</div>
-                <div >{dayjs(product.endAt).format("MM/DD")}</div>
-                </div>
-              </Content>
-            </PrimaryAction>
-          </Card>
-        </div>
-      {/each}
-    </div>
+                  <div class="text-lg text-[#4A4A4A] mt-1 flex justify-center">
+                    予約期間：
+                    <div>{dayjs(product.startAt).format("MM/DD")}</div>
+                    <div>〜</div>
+                    <div>{dayjs(product.endAt).format("MM/DD")}</div>
+                  </div>
+                </Content>
+              </PrimaryAction>
+            </Card>
+          </div>
+        {/each}
+      </div>
     </div>
   </div>
 {/await}
